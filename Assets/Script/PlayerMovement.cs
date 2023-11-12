065aa2c5c190ae849ffa4f19c25804500f25e615
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-11-12 19:19:25 +0800

    Revert "added PlayerAnimator script"

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -30,8 +30,4 @@ public class PlayerMovement : MonoBehaviour
     void Move(){
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
-
-    public float GetHorizontalInput(){
-        return moveDirection.x;
-    }
 }
